fea3a02b3f22375fb2de5a0c8be9a47db4872e32
Tatiana Slonovskaya <tatianaslonovskaya@gmail.com>
2022-12-16 20:46:15 +0100

    Markup and styes added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,64 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <div>
+
+        {/* Search bar*/}
+        <div className="Search">
+          <form className="search-bar">
+            <input
+              type="text"
+              className="search"
+              id="search-input"
+              placeholder="Search"
+              autofocus
+            />
+            <input type="submit" class="search-btn" value="🔍" />
+          </form>
+        </div>
+
+        {/* Main content */}
+        <div className="MainContent">
+          <div className="temp">
+            <span id="celsius">°C </span>/<span id="fahrenheit"> °F</span>
+          </div>
+
+          <div className="text">
+            <h1>
+              <span className="city" id="city">
+                Nikopol
+              </span>{" "}
+              <br />
+              <span id="temperature" className="degree">
+                8°C
+              </span>
+              <br />
+            </h1>
+            <p>Saturday, 26th<br /> November 2022</p>
+            <p id="description"></p>
+          </div>
+          <div className="text">
+            <ul>
+              <li id="humidity">Humidity: 23%</li>
+              <li id="wind">Wind: 3 Km/H</li>
+            </ul>
+          </div>
+          <div className="icon">
+            <img
+              src="http://openweathermap.org/img/wn/01d@2x.png"
+              alt="Icon"
+              id="icon"
+            />
+          </div>
+        </div>
+
+        
+
+      </div>
     </div>
+
   );
 }
 
---
+++ src/GithubLink.js
@@ -0,0 +1,15 @@
+import "./GithubLink.css";
+
+export default function GithubLink() {
+    return (
+        <div className="GithubLink">
+            <button class="github">
+                <a
+                    href="https://github.com/hollyts123/weather-react"
+                    target="_blank"
+                    rel="noreferrer"
+                >Go to Github</a>
+            </button>
+        </div>
+    );
+}
